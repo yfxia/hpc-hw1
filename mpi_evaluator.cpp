63a2c9e60ac15c23ade8541b06d6b03925b07afa
Yunfei Xia <yxia90@jinx-login.cc.gatech.edu>
2017-02-26 13:53:02 -0500

    first_two

--- mpi_evaluator.cpp
+++ mpi_evaluator.cpp
@@ -8,15 +8,52 @@
 
 #include "mpi_evaluator.h"
 #include "const.h"
+#include "<math.h>"
 
 void scatter(const int n, double* scatter_values, int &n_local, double* &local_values, int source_rank, const MPI_Comm comm){
     //Implementation
-
+    // double d = log2(p);
+    //Configure d using the right shift bitwise operator
+    int d = 0;
+    int value = source_rank;
+    while (value > 0) {
+	d++;
+	value = value >> 1;
+    }
+    for (j = 0; j < d; j = j + 1){
+	double mask = double pow(2, j);
+	if ((source_rank AND mask != 0 )){
+		//if it is the last processor, use floor, else ceiling
+		count = ceil(n/p); 
+		MPI_Send(scatter_values, count, MPI_INT,
+			0, 111, comm); 
+	} else {
+		MPI_Status stat;
+		MPI_Recv(scatter_values, count, MPI_INT, 
+			0, 111, comm, &stat);
+	}
+	
+    }
+    return MPI_Finalize();
 }
 
+//Broadcast a real number among p processors
 double broadcast(double value, int source_rank, const MPI_Comm comm){
     //Implementation
-
+    int world_rank;
+    //MPI_Init(&argc, &argv);
+    MPI_Comm_rank(comm, &world_rank);
+    //If at processor 0, send data to everyone
+    if (world_rank == source_rank){
+	for (int i = 0; i < world_size; i++){
+		if (i != source_rank){
+			MPI_Send(value, 1, MPI_DOUBLE, i, 0, comm);
+		}
+	} 
+    } else {
+		MPI_Recv(value, 1, MPI_DOUBLE, source_rank, 0, comm, MPI_STATUS_IGNORE);
+	}
+    }
     return 0;
 }
 
